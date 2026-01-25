5346c62a5dd2b6bca6e3c69549e0966e5dd80e42
felipecartaxo <felipecartaxo1@gmail.com>
2026-01-25 14:18:22 -0300

    feat: implementa novas consultas para atender aos demais requisitos funcionais

--- src/main/java/br/edu/ifpb/veritas/repositories/MeetingRepository.java
+++ src/main/java/br/edu/ifpb/veritas/repositories/MeetingRepository.java
@@ -3,9 +3,12 @@ package br.edu.ifpb.veritas.repositories;
 import br.edu.ifpb.veritas.enums.MeetingStatus;
 import br.edu.ifpb.veritas.models.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
@@ -26,4 +29,14 @@ public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 
     // Busca reuniões de um colegiado onde o professor está escalado
     List<Meeting> findByCollegiateIdAndParticipantsId(Long collegiateId, Long professorId);
+
+    // Busca reunião ativa no momento (lembrando que apenas uma pode estar ativa)
+    Optional<Meeting> findByActiveTrue();
+
+    // REQFUNC 9: Busca reuniões de um colegiado ordenadas por data agendada
+    List<Meeting> findByCollegiateIdOrderByScheduledDateDesc(Long collegiateId);
+
+    // Busca reuniões que contenham um processo específico na pauta
+    @Query("SELECT m FROM Meeting m JOIN m.processes p WHERE p.id = :processId")
+    List<Meeting> findByProcessInAgenda(@Param("processId") Long processId);
 }
