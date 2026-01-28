a97013b1174e1d0ea35f937bbbb2e2b288c3678d
felipecartaxo <felipecartaxo1@gmail.com>
2026-01-28 00:48:40 -0300

    fix: ajusta o tipo da coluna referente ao documento no postgres e atualiza o dataseeder

--- src/main/java/br/edu/ifpb/veritas/models/Process.java
+++ src/main/java/br/edu/ifpb/veritas/models/Process.java
@@ -22,74 +22,73 @@ import lombok.Setter;
 @Table(name = "TB_PROCESSES")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Process {
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-   @Column(name = "TITLE")
-   private String title;
+    @Column(name = "TITLE")
+    private String title;
 
-   @Column(name = "DESCRIPTION")
-   private String description;
+    @Column(name = "DESCRIPTION")
+    private String description;
 
-   @Column(name = "NUMBER")
-   private String number;
+    @Column(name = "NUMBER")
+    private String number;
 
-   @Column(name = "CREATED_AT")
-   private LocalDateTime createdAt;
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
 
-   @Column(name = "DISTRIBUTED_AT")
-   private LocalDateTime distributedAt;
+    @Column(name = "DISTRIBUTED_AT")
+    private LocalDateTime distributedAt;
 
-   @Column(name = "SOLVED_AT")
-   private LocalDateTime solvedAt;
+    @Column(name = "SOLVED_AT")
+    private LocalDateTime solvedAt;
 
-   @Column(name = "PROCESS_STATUS")
-   @Enumerated(EnumType.STRING)
-   private StatusProcess status;
+    @Column(name = "PROCESS_STATUS")
+    @Enumerated(EnumType.STRING)
+    private StatusProcess status;
 
-   @Column(name = "RAPPORTEUR_VOTE")
-   @Enumerated(EnumType.STRING)
-   private DecisionType rapporteurVote;
+    @Column(name = "RAPPORTEUR_VOTE")
+    @Enumerated(EnumType.STRING)
+    private DecisionType rapporteurVote;
 
-   @Column(name = "PROCESS_ORDER")
-   private String processOrder;
+    @Column(name = "PROCESS_ORDER")
+    private String processOrder;
 
-   @ManyToOne
-   @JoinColumn(name = "INTERESTED_STUDENT_ID", nullable = false)
-   private Student processCreator;
+    @ManyToOne
+    @JoinColumn(name = "INTERESTED_STUDENT_ID", nullable = false)
+    private Student processCreator;
 
-   @ManyToOne
-   @JoinColumn(name = "PROCESS_SUBJECT_ID", nullable = false)
-   private Subject subject;
+    @ManyToOne
+    @JoinColumn(name = "PROCESS_SUBJECT_ID", nullable = false)
+    private Subject subject;
 
-   @ManyToOne
-   @JoinColumn(name = "PROFESSOR_RAPPORTEUR_ID", nullable = true)
-   private Professor processRapporteur;
+    @ManyToOne
+    @JoinColumn(name = "PROFESSOR_RAPPORTEUR_ID", nullable = true)
+    private Professor processRapporteur;
 
-   @OneToMany(mappedBy = "process", cascade = CascadeType.ALL)
-   private List<Vote> processVoteList;
+    @OneToMany(mappedBy = "process", cascade = CascadeType.ALL)
+    private List<Vote> processVoteList;
 
-   /**
-    * Justificativa escrita pelo relator ao votar (REQFUNC 5)
-    */
-   @Column(name = "RAPPORTEUR_JUSTIFICATION", columnDefinition = "TEXT")
-   private String rapporteurJustification;
+    /**
+     * Justificativa escrita pelo relator ao votar (REQFUNC 5)
+     */
+    @Column(name = "RAPPORTEUR_JUSTIFICATION", columnDefinition = "TEXT")
+    private String rapporteurJustification;
 
-   /**
-    * REQFUNC 16: Armazena o PDF do requerimento enviado pelo aluno
-    */
-   @Lob
-   @Column(name = "DOCUMENT", columnDefinition = "BYTEA")
-   private byte[] document;
+    /**
+     * REQFUNC 16: Armazena o PDF do requerimento enviado pelo aluno
+     */
+    @Column(name = "DOCUMENT", columnDefinition = "BYTEA")
+    private byte[] document;
 
-   @Column(name = "DOCUMENT_FILENAME")
-   private String documentFilename;
+    @Column(name = "DOCUMENT_FILENAME")
+    private String documentFilename;
 
-   @Column(name = "DOCUMENT_UPLOAD_DATE")
-   private LocalDateTime documentUploadDate;
+    @Column(name = "DOCUMENT_UPLOAD_DATE")
+    private LocalDateTime documentUploadDate;
 
-   /**
-    * Status atual do processo no fluxo
-    */
+    /**
+     * Status atual do processo no fluxo
+     */
 }
